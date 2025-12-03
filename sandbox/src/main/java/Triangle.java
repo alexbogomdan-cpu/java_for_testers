45e677241595cb89c019f6f1ba534c12fe57713d
Aleksey Bogdan <alexbogomdan@gmail.com>
2025-12-03 20:30:56 +0300

    Создал объекты ,и переписал функции под эти объекты

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,4 +1,16 @@
 public class Triangle {
+
+    public double side_a;
+    public double side_b;
+    public double side_c;
+
+
+    public Triangle(double side_a, double side_b, double side_c) {
+        this.side_a = side_a;
+        this.side_b = side_b;
+        this.side_c = side_c;
+    }
+
     public static void main(String[] args) {
         printTrianglePerimeter(5.0,6.0,7.0);
         printTriangleAria(5,6,7);
@@ -20,5 +32,13 @@ public class Triangle {
 
     public static double trianglAria(double a, double b, double c) {
         return Math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+    }
+
+    public double Perimeter() {
+        return this.side_a + this.side_b + this.side_c;
+    }
+
+    public double Aria() {
+        return Math.sqrt(((side_a + side_b + side_c) / 2) * (((side_a + side_b + side_c) / 2) - side_a) * (((side_a + side_b + side_c) / 2) - side_b) * (((side_a + side_b + side_c) / 2) - side_c));
     }
 }
--- sandbox/src/test/java/TriangleTests.java
+++ sandbox/src/test/java/TriangleTests.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
     @Test
     void canCalculatePerimeter(){
-        double result = Triangle.trianglePerimeter(5,6,7);
+        var t = new Triangle(5,6,7);
+        double result = t.Perimeter();
         Assertions.assertEquals(18,result);
 
     }
     @Test
     void canCalulateArea(){
-        double result = Triangle.trianglAria(5,6,7);
+        var t = new Triangle(5,6,7);
+        double result = t.Aria();
         Assertions.assertEquals(14.696938456699069,result);
     }
 }
