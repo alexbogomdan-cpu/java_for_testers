ec090f1cacf7743f2e3421832a94bdd46dfab640
baekrbr <alexbogomdan@gmail.com>
2025-12-31 08:25:11 +0300

    Написал параметризованные тесты для создания групп

--- addressbook_web_tests/src/test/java/Tests/GroupCreationsTests.java
+++ addressbook_web_tests/src/test/java/Tests/GroupCreationsTests.java
@@ -14,23 +14,57 @@ import java.util.stream.Stream;
 
 public class GroupCreationsTests extends TestBase {
 
-    public static List<String> groupNameProvider() {
-        var result = new ArrayList<String>();// создаем пустой список
-        for (int i = 0; i < 5; i++ ){//цикл для повторяния действия,которое в фигурных скобках
-            result.add(randomString(i * 10));
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>();
+        for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++) {//цикл для повторяния действия,которое в фигурных скобках
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));//добавляем объекты типа GroupDate со случано сгенерированным name,header,footer
         }
         return result;
-
     }
 
-    @ParameterizedTest// создали параметролизованный тест
-    @ValueSource(strings = {"group name","group name'"})
-    public void CanCreateGroup(String name) {
+    @ParameterizedTest
+    @MethodSource("groupProvider")
+    public void CanCreateMultiplelGroup(GroupData group) {//создали тест,который имеет один параметр ввиде обьекта GroupDate
         int groupCount = app.groups().getCount();//Подсчитываем колличество груп переред созданием
-        TestBase.app.groups().createGroup(new GroupData(name, "test", "test"));//создаем новую группу
+        app.groups().createGroup(group);//создаем новую группу, которая передается в качестве параметров тестируемой функции
         int newGroupCount = app.groups().getCount();//подсчитываем коллчичество груп после добавления группы
         Assertions.assertEquals(groupCount + 1,newGroupCount);
     }
+
+
+
+
+    public static List<GroupData> negativeGroupProvider() {
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData("group name'","","")));// создаем пустой список
+        return result;
+
+    }
+    @ParameterizedTest
+    @MethodSource("negativeGroupProvider")
+    public void CanNotCreateGroup(GroupData group) {//создали тест,который имеет один параметр ввиде обьекта GroupDate
+        int groupCount = app.groups().getCount();//Подсчитываем колличество груп переред созданием
+        app.groups().createGroup(group);//создаем новую группу, которая передается в качестве параметров тестируемой функции
+        int newGroupCount = app.groups().getCount();//подсчитываем коллчичество груп после добавления группы
+        Assertions.assertEquals(groupCount ,newGroupCount);//сравниваем количество груп до и после
+    }
+
+
+//    @ParameterizedTest// создали параметролизованный тест
+//    @ValueSource(strings = {"group name","group name'"})
+//    public void CanCreateGroup(String name) {
+//        int groupCount = app.groups().getCount();//Подсчитываем колличество груп переред созданием
+//        TestBase.app.groups().createGroup(new GroupData(name, "test", "test"));//создаем новую группу
+//        int newGroupCount = app.groups().getCount();//подсчитываем коллчичество груп после добавления группы
+//        Assertions.assertEquals(groupCount + 1,newGroupCount);
+//    }
 
 
 //    @Test
@@ -42,24 +76,27 @@ public class GroupCreationsTests extends TestBase {
 //    }
 
 
-    @Test
-    void canCreateGroupWithEmtyName() {
-        TestBase.app.groups().createGroup(new GroupData());
-    }
+//    @Test
+//    void canCreateGroupWithEmtyName() {
+//        TestBase.app.groups().createGroup(new GroupData());
+//    }
+//
+//    @Test
+//    void canCreateGroupWithNameOnly() {
+//        TestBase.app.groups().createGroup(new GroupData().withName("some name"));
+//    }
 
-    @Test
-    void canCreateGroupWithNameOnly() {
-        TestBase.app.groups().createGroup(new GroupData().withName("some name"));
-    }
+//    @ParameterizedTest
+//    @MethodSource("groupNameProvider")
+//    public void CanCreateMultiplelGroup(String name) {
+//        int groupCount = app.groups().getCount();//Подсчитываем колличество груп переред созданием
+//        app.groups().createGroup(new GroupData(name, "test", "test"));//создаем новую группу
+//        int newGroupCount = app.groups().getCount();//подсчитываем коллчичество груп после добавления группы
+//        Assertions.assertEquals(groupCount + 1,newGroupCount);
+//    }
 
-    @ParameterizedTest
-    @MethodSource("groupNameProvider")
-    public void CanCreateMultiplelGroup(String name) {
-        int groupCount = app.groups().getCount();//Подсчитываем колличество груп переред созданием
-        app.groups().createGroup(new GroupData(name, "test", "test"));//создаем новую группу
-        int newGroupCount = app.groups().getCount();//подсчитываем коллчичество груп после добавления группы
-        Assertions.assertEquals(groupCount + 1,newGroupCount);
-    }
+
+
 
 
 //    @Test
